213a676a2b275d789be95a05121247f1549466b6
Daniel Pritsker <danielpr137@gmail.com>
2024-06-05 11:46:01 +0300

    add time calc

--- app/routes.py
+++ app/routes.py
@@ -1,33 +1,22 @@
-from flask import current_app, jsonify, request, abort
 from app import app, db
 from app.models import VirtualMachine
 from app.cloud_env import get_attackers
-from app.utils import timed_function
+from app.utils import get_attackers
+from flask import Blueprint, request, jsonify, current_app
+
 
 # Global variables to store statistics
 request_count = 0
 total_request_time = 0
 
-@app.route('/api/v1/attack', methods=['GET'])
-# @timed_function
-def attack(execution_time=None):
-    global request_count, total_request_time
+
+api_bp = Blueprint('api', __name__)
+
+@api_bp.route('/attack', methods=['GET'])
+def attack():
     vm_id = request.args.get('vm_id')
-    app.logger.info(f'Received attack request for VM ID: {vm_id}')
-
-    attackers, error_message = get_attackers(vm_id)
-    if error_message:
-        app.logger.warning(error_message)
-        abort(404, error_message)
-
-    request_count += 1
-    total_request_time += execution_time or 0  # Add the execution time to the total
-
-    if attackers:
-        app.logger.info(f'Found {len(attackers)} attackers for VM ID: {vm_id}')
-    else:
-        app.logger.warning(f'No attackers found for VM ID: {vm_id}')
-
+    cloud_env = current_app.config['cloud_env']
+    attackers = get_attackers(vm_id, cloud_env)
     return jsonify(attackers)
 
 @app.route('/api/v1/stats', methods=['GET'])
--- app/utils.py
+++ app/utils.py
@@ -7,11 +7,24 @@ def can_attack(attacker_tags, target_tags, fw_rules):
             return True
     return False
 
-def timed_function(func):
-    def wrapper(*args, **kwargs):
-        start_time = time.time()
-        result = func(*args, **kwargs)
-        end_time = time.time()
-        execution_time = end_time - start_time
-        return result, execution_time
-    return wrapper
+def get_vm_ids(cloud_env):
+    return [vm['vm_id'] for vm in cloud_env['vms']]
+
+def get_attackers(vm_id, cloud_env):
+    attackers = []
+    vm_tags = {vm['vm_id']: vm['tags'] for vm in cloud_env['vms']}
+    fw_rules = cloud_env['fw_rules']
+    
+    if vm_id not in vm_tags:
+        return attackers
+    
+    dest_tags = set(vm_tags[vm_id])
+    
+    for rule in fw_rules:
+        if rule['dest_tag'] in dest_tags:
+            source_tag = rule['source_tag']
+            for vm in cloud_env['vms']:
+                if source_tag in vm['tags'] and vm['vm_id'] != vm_id:
+                    attackers.append(vm['vm_id'])
+    
+    return attackers
